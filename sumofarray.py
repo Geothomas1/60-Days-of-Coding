e63e293b2ccf734c08de043e772ed2b288a1524c
geothomas <geothomas@cet.ac.in>
2020-09-27 16:06:27 +0530

    Array sum

---
+++ sumofarray.py
@@ -0,0 +1,9 @@
+"""Given an array of integer, Find the sum of its elements.
+For example, If the array ar = [1,2,3], 1+2+3=6, so return 6.
+"""
+import numpy as d
+s=0
+arr=d.array([10,20,30])
+for i in arr:
+    s=s+i
+print("sum of array",i)
